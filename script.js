3b87777e10b266f32052b3432c8faba1dc3ffa16
andreantama96 <andreanyogatama1@gmail.com>
2020-05-12 23:39:24 -0700

    Line 17 tidak perlu dideklarasikan kembali variabel numbers, karena sudah dideklarasikan di line 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const numbers= document.querySelectorAll(".number")
+const numbers= document.querySelectorAll(".number");
     
 
 numbers.forEach((number) => {
@@ -14,19 +14,19 @@ const updateScreen = (number) => {
     calculatorScreen.value = number
 }
 
-const numbers = document.querySelectorAll(".number")
+//const numbers = document.querySelectorAll(".number")
 
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
         updateScreen(event.target.value)
     })
 })
-s
+
 let prevNumber = ''
 let calculationOperator = ''
 let currentNumber = '0'
 
-const inputNumber = (number) => {
+let inputNumber = (number) => {
     currentNumber = number
 }
 
@@ -37,11 +37,11 @@ numbers.forEach((number) => {
     })
 })
 
-const inputNumber= (number) => {
+inputNumber= (number) => {
     currentNumber += number
 }
 
-if inputNumber = (number) => {
+inputNumber = (number) => {
     if(currentNumber === '0') {
         currentNumber = number
     } else {
